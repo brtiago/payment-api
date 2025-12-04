5c840f49c35546316793d017457075b26b9c6694
brtiago <tgribeiro@gmail.com>
2025-12-04 04:23:08 -0300

    feat: criando classe buyer response

--- src/main/java/com/wirecard/challenge/paymentsapi/dto/BuyerResponse.java
+++ src/main/java/com/wirecard/challenge/paymentsapi/dto/BuyerResponse.java
@@ -1,5 +1,17 @@
 package com.wirecard.challenge.paymentsapi.dto;
 
-public record BuyerResponse() {
+import com.wirecard.challenge.paymentsapi.model.Buyer;
 
+public record BuyerResponse(
+        String email,
+        String cpf,
+        String nome
+) {
+    public static BuyerResponse fromEntity(Buyer entidade) {
+        return new BuyerResponse(
+                entidade.getEmail(),
+                entidade.getCpf(),
+                entidade.getNome()
+        );
+    }
 }
