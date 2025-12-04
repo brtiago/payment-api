64c4bb6bec757b154e7107e41e27d4d5727fa932
brtiago <tgribeiro@gmail.com>
2025-12-04 05:00:11 -0300

    feat: criando metodo deletarBuyer

--- src/main/java/com/wirecard/challenge/paymentsapi/service/BuyerService.java
+++ src/main/java/com/wirecard/challenge/paymentsapi/service/BuyerService.java
@@ -4,6 +4,7 @@ import com.wirecard.challenge.paymentsapi.dto.BuyerRequest;
 import com.wirecard.challenge.paymentsapi.dto.BuyerResponse;
 import com.wirecard.challenge.paymentsapi.model.Buyer;
 import com.wirecard.challenge.paymentsapi.repository.BuyerRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,13 @@ public class BuyerService {
         Buyer entidade = new Buyer(request);
         Buyer salvo = buyerRepository.save(entidade);
         return BuyerResponse.fromEntity(salvo);
+    }
+
+    @Transactional
+    public void deletarBuyer(Long id) {
+        Buyer buyer = buyerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Buyer não encontrado"));
+        buyerRepository.delete(buyer);
     }
 
 }
