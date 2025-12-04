c31bd795a9b7a1b68402bedd9bdf0475a14cfec5
brtiago <tgribeiro@gmail.com>
2025-12-04 04:25:14 -0300

    feat: criando classe buyer service com método listaBuyers

---
+++ src/main/java/com/wirecard/challenge/paymentsapi/service/BuyerService.java
@@ -0,0 +1,24 @@
+package com.wirecard.challenge.paymentsapi.service;
+
+import com.wirecard.challenge.paymentsapi.dto.BuyerResponse;
+import com.wirecard.challenge.paymentsapi.repository.BuyerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BuyerService {
+
+    private final BuyerRepository buyerRepository;
+    public BuyerService(BuyerRepository buyerRepository) {
+        this.buyerRepository = buyerRepository;
+    }
+
+    public List<BuyerResponse> listaBuyers() {
+        return buyerRepository.findAll()
+                .stream()
+                .map(BuyerResponse::fromEntity)
+                .toList();
+    }
+
+}
