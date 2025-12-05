b7ff0ed912eb1fbee027e9b55a05c9566cd76458
brtiago <tgribeiro@gmail.com>
2025-12-05 16:47:05 -0300

    refactor: create boleto returning response entity

--- src/main/java/com/wirecard/challenge/paymentsapi/controller/BoletoController.java
+++ src/main/java/com/wirecard/challenge/paymentsapi/controller/BoletoController.java
@@ -1,6 +1,13 @@
 package com.wirecard.challenge.paymentsapi.controller;
 
+import com.wirecard.challenge.paymentsapi.dto.BoletoRequest;
+import com.wirecard.challenge.paymentsapi.dto.BoletoResponse;
 import com.wirecard.challenge.paymentsapi.model.Boleto;
+import com.wirecard.challenge.paymentsapi.service.BoletoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Classe responsável por controlar operações e requisições referentes aos Boletos
@@ -9,15 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/boleto")
 public class BoletoController {
-	
-	public Boleto createBoleto(String code, String expDate) {
-		Boleto boleto = new Boleto();
-		boleto.setCode(code);
-		boleto.setExpirationDate(expDate);
-		
-		return boleto;
-		
-	}
+
+    private final BoletoService boletoService;
+    public BoletoController(BoletoService boletoService) {
+        this.boletoService = boletoService;
+    }
+
+    public ResponseEntity<BoletoResponse> createBoleto(@RequestBody BoletoRequest request) {
+        BoletoResponse boletoCriado = boletoService.createBoleto(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(boletoCriado);
+    }
+
+    public Boleto createBoleto(String code, String expDate) {
+        return new Boleto(code, expDate);
+    }
 
 }
