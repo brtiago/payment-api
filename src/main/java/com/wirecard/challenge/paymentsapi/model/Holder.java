aaeb4f1eea4947ba4c53fbc27518f0ac4749a12c
brtiago <tgribeiro@gmail.com>
2025-12-07 14:15:22 -0300

    refactor: refatorando Holder

--- src/main/java/com/wirecard/challenge/paymentsapi/model/Holder.java
+++ src/main/java/com/wirecard/challenge/paymentsapi/model/Holder.java
@@ -1,69 +1,32 @@
 package com.wirecard.challenge.paymentsapi.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 //Classe que representa o proprietário do Cartão de Crédito//
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Holder {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(name="name")
+
 	private String name;
 	
 	// Data de Nascimento //
 	
-	@Column(name="birthDate")
 	private String birthDate;
 	
 	//Número do documento //
 	
-	@Column(name="documentNumber")
 	private String documentNumber;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(String birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getDocumentNumber() {
-		return documentNumber;
-	}
-
-	public void setDocumentNumber(String documentNumber) {
-		this.documentNumber = documentNumber;
-	}
-
-	@Override
-	public String toString() {
-		return "Holder [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", documentNumber=" + documentNumber
-				+ "]";
-	}
-	
-	
 }
