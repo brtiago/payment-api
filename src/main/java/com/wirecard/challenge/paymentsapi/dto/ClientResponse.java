e1f8b32783f4769fdf08920af99114334fc8a2a5
brtiago <tgribeiro@gmail.com>
2025-12-10 18:00:12 -0300

    refactor: removendo método fromEntity

--- src/main/java/com/wirecard/challenge/paymentsapi/dto/ClientResponse.java
+++ src/main/java/com/wirecard/challenge/paymentsapi/dto/ClientResponse.java
@@ -1,15 +1,7 @@
 package com.wirecard.challenge.paymentsapi.dto;
 
-import com.wirecard.challenge.paymentsapi.model.Client;
-
 public record ClientResponse(
         Long id,
         String name
 ) {
-    public static ClientResponse fromEntity (Client entidade) {
-        return new ClientResponse(
-                entidade.getId(),
-                entidade.getName()
-                );
-    }
 }
